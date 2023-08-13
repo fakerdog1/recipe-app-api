04651ed81727d97f421dc2212c5d99e7fe628d06
Martin <simeonoff.martin@gmail.com>
2023-08-13 16:46:04 +0300

    private authentication

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -30,6 +30,17 @@ class UserSerializer(serializers.ModelSerializer):
         """Create and return a user with encrypted paswsword"""
         return get_user_model().objects.create_user(**validated_data)
     
+    def update(self, instance, validated_data):
+        """Update and return a user"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+        
+        if password:
+            user.set_password(password)
+            user.save()
+            
+        return user
+    
 class AuthTokenSerializer(serializers.Serializer):
     """Serliazier for the user authentication object"""
     email = serializers.EmailField()

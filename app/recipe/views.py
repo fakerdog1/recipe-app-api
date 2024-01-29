610e4fc3b4baabf41beebcbe40cbbd13447b6b06
fakerdog1 <simeonoff.martin@gmail.com>
2024-01-29 18:52:19 +0200

    ingredient api setup

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -7,7 +7,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from recipe import serializers
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """Manage recipes in db"""
@@ -45,5 +45,21 @@ class TagViewSet(
     
     def get_queryset(self):
         """Return tags for authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+    
+class IngredientViewSet(
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
+    """Manage ingredients in db"""
+    serializer_class = serializers.IngredientSerializer
+    queryset=Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def get_querysest(self):
+        """Return ingredients for authenticated user"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
     
